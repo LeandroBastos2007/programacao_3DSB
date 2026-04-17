c6d82475e95f132c6a67c6e44c32ac5944c1afc5
LeandroBastos2007 <leandro.servello.santos@escola.pr.gov.br>
2026-04-17 09:25:51 -0300

    Update verificador.py

--- verificador.py
+++ verificador.py
@@ -8,3 +8,20 @@ if numero % 2 == 0:
 else:
     # Se for ímpar, imprime a mensagem
     print(f"{numero} é ______________.")
+
+# Solicita um número ao usuário
+numero = float(input("__________________________"))
+
+# Verifica se o número é positivo
+if numero > 0:
+    # Se for positivo, imprime a mensagem
+    print("O número é _________________.")
+# Verifica se o número é negativo
+elif numero < 0:
+    # Se for negativo, imprime a mensagem
+    print("_______________________________________")
+# Se não for positivo nem negativo, é zero
+else:
+    # Imprime a mensagem para zero
+    print("__________________________________")
+
